de00393a0915c02609843e1689d8daed4d19b4e5
Ryota <ryoenoki2013@gmail.com>
2022-02-03 20:06:46 +0900

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import trollFace from "../images/troll-face.png";
 
 export default function Header(){
     return(
-      <header>
-        <h1>
-          Expensify
-        </h1>
+      <header className="header">
+        <img src={trollFace} alt="troll face" className="header__image"/>
+        <h2 className="header__title">Meme Generator</h2>
+        <h4 className="header__project">React Course - Project 3</h4>
       </header>
     )
 }
