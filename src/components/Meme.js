37fcdbd3817663c073104fefb8c67ece60148a7e
Ryota <ryoenoki2013@gmail.com>
2022-02-06 00:04:56 +0900

    form added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+export default function Meme(){
+  return(
+  <main>
+    <form className="form">
+      <input type="text" name="topText" placeholder="Top Text" className="form__input"/>
+      <input type="text" name="bottomText" placeholder="Bottom Text" className="form__input"/>
+      <button className="form__button">Get a new meme image 🖼</button>
+    </form>
+  </main>
+  )
+}
