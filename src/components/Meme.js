bc845ce22a9c04201894bfda00bdad361577c412
Ryota <ryoenoki2013@gmail.com>
2022-02-06 01:03:23 +0900

    added onclick event to button and created function called getMemeImage

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 export default function Meme(){
+  function getMemeImage(){
+    console.log("getMemeImage");
+  }
   return(
   <main>
-    <form className="form">
+    <div className="form">
       <input type="text" name="topText" placeholder="Top Text" className="form__input"/>
       <input type="text" name="bottomText" placeholder="Bottom Text" className="form__input"/>
-      <button className="form__button">Get a new meme image 🖼</button>
-    </form>
+      <button className="form__button" onClick={getMemeImage}>Get a new meme image 🖼</button>
+    </div>
   </main>
   )
 }
